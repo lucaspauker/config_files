50169bca0ad3d0236b6d5f77d3423165e365ec19
Lucas Pauker <lucas.pauker@gmail.com>
2021-10-17 15:13:30 -0700

    Add dir variable

--- setup.sh
+++ setup.sh
@@ -1,29 +1,31 @@
+DIR=~
+
 echo "vimrc"
-rm ~/.vimrc
-cp ./.vimrc ~/
+rm $DIR/.vimrc
+cp ./.vimrc $DIR
 
 echo "vim directory"
-rm -rf ~/.vim
-cp -r ./.vim ~/
+rm -rf $DIR/.vim
+cp -r ./.vim $DIR
 
 echo "ssh"
-rm -rf ~/.ssh
-cp -r ./.ssh ~/
+rm -rf $DIR/.ssh
+cp -r ./.ssh $DIR
 
 echo "bash_profile"
-rm  ~/.bash_profile
-cp ./.bash_profile ~/
+rm  $DIR/.bash_profile
+cp ./.bash_profile $DIR
 
 echo "iterm2_shell_integration"
-rm ~/.iterm2_shell_integration.bash
-cp ./.iterm2_shell_integration.bash ~/
+rm $DIR/.iterm2_shell_integration.bash
+cp ./.iterm2_shell_integration.bash $DIR
 
 echo "gitconfig"
-rm ~/.gitconfig
-cp ./.gitconfig ~/
+rm $DIR/.gitconfig
+cp ./.gitconfig $DIR
 
 echo "source"
-source ~/.iterm2_shell_integration.bash
-source ~/.bash_profile
+source $DIR/.iterm2_shell_integration.bash
+source $DIR/.bash_profile
 
 echo "setup complete"
